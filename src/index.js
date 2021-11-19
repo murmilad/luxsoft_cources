501e3fa4ba16649368093c2e507d948216bf94c1
alexey <murmilad@gmail.com>
2021-11-19 22:11:40 +0300

    first cources

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Containers/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter} from "react-router-dom";
+import { Provider } from 'react-redux';
+import configureStore from './Store/index';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+		    		<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
   document.getElementById('root')
 );
 
